4cb94942538bf455e6a135114574e460a81291c5
Parag Panchal <Parag.Panchal@entura.com.au>
2019-04-18 17:09:37 +1000

    a new class TestClass was created

---
+++ WorkflowEngine/TempClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkflowEngine
+{
+    class TempClass
+    
+        //this class was created to test the git hub functainility
+    }
+}
